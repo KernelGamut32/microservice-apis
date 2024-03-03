1d974540ad29453ca4c6979254bcd7160843c1b5
Allen Sanders <asanders@gamuttechnologysvcs.com>
2024-03-03 14:56:55 -0500

    updated source code and gitignore

--- ch09/client.py
+++ ch09/client.py
@@ -1,15 +1,53 @@
 import requests
 
-URL = 'http://localhost:9002/graphql'
+URL = "http://localhost:9002/graphql"
 
-query_document = '''
+query_document = """
 {
   allIngredients {
     name
   }
 }
-'''
+"""
 
-result = requests.get(URL, params={'query': query_document})
+query_result = requests.get(URL, params={"query": query_document})
 
-print(result.json())
+print(query_result.json())
+
+mutation_document = """
+mutation CreateAndDeleteProduct(
+  $name: String!
+  $type: ProductType!
+  $input: AddProductInput!
+  $id: ID!
+) {
+  addProduct(name: $name, type: $type, input: $input) {
+    ...commonProperties
+  }
+  deleteProduct(id: $id)
+}
+ 
+fragment commonProperties on ProductInterface {
+  name
+}
+"""
+
+mutation_variables = {
+    "name": "Latte",
+    "type": "beverage",
+    "input": {
+        "price": 12.50,
+        "size": "BIG",
+        "ingredients": [{"ingredient": 1, "quantity": 1, "unit": "LITERS"}],
+    },
+    "id": "Mocha",
+}
+
+payload = {
+    "query": mutation_document,
+    "variables": mutation_variables,
+}
+
+mutation_result = requests.post(URL, json=payload)
+
+print(mutation_result.json())
