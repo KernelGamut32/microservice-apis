bfcd2f4a7f3d842ca3e5cd51452803252095507b
Allen Sanders <asanders@gamuttechnologysvcs.com>
2024-03-07 07:54:29 -0500

    updated ch11 implementation

--- ch11/jwt_generator.py
+++ ch11/jwt_generator.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from pathlib import Path
+import uuid
 
 import jwt
 from cryptography.hazmat.primitives import serialization
@@ -8,15 +9,15 @@ from cryptography.hazmat.primitives import serialization
 def generate_jwt():
     now = datetime.utcnow()
     payload = {
-        "iss": "https://auth.coffeemesh.io/",
-        "sub": "ec7bbccf-ca89-4af3-82ac-b41e4831a962",
+        "iss": "https://auth.mycompany.io/",
+        "sub": str(uuid.uuid4()),
         "aud": "http://127.0.0.1:8000/orders",
         "iat": (now + timedelta(hours=-5)).timestamp(),
         "exp": (now + timedelta(hours=24)).timestamp(),
         "scope": "openid",
     }
 
-    private_key_text = Path("private_key.pem").read_text()
+    private_key_text = Path("new_key.pem").read_text()
     private_key = serialization.load_pem_private_key(
         private_key_text.encode(),
         password=None,
